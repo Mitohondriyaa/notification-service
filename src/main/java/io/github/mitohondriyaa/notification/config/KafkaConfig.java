f9e8ffabf29c5a5520865a058a53a69ca5cf0770
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-28 12:55:18 +0300

    Fix some mistakes

--- src/main/java/io/github/mitohondriyaa/notification/config/KafkaConfig.java
+++ src/main/java/io/github/mitohondriyaa/notification/config/KafkaConfig.java
@@ -1,4 +1,4 @@
-package io.github.mitohondriyaa.config;
+package io.github.mitohondriyaa.notification.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
--- src/main/java/io/github/mitohondriyaa/notification/config/SecurityConfig.java
+++ src/main/java/io/github/mitohondriyaa/notification/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package io.github.mitohondriyaa.config;
+package io.github.mitohondriyaa.notification.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
