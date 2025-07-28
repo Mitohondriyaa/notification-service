15978842ae27114024b467c13b6370113b6ad8df
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-28 12:07:27 +0300

    Add kafka error handling

---
+++ src/main/java/io/github/mitohondriyaa/config/KafkaConfig.java
@@ -0,0 +1,42 @@
+package io.github.mitohondriyaa.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Slf4j
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        FixedBackOff fixedBackOff = new FixedBackOff(1000, 3);
+
+        return new DefaultErrorHandler(
+            (record, exception) -> {
+                log.error("Kafka error. Topic: {}, Key: {}, Value: {}, Exception: {}",
+                    record.topic(),
+                    record.key(),
+                    record.value(),
+                    exception.getMessage()
+                );
+            },
+            fixedBackOff
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+        ConsumerFactory<String, Object> consumerFactory,
+        DefaultErrorHandler errorHandler
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
+
+        return factory;
+    }
+}
