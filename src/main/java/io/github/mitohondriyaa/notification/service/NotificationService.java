a2abb1dfe9bb97a86b7cac331230132f7c8a8950
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-30 13:51:07 +0300

    Add logic to notification service method to reach idempotency

--- src/main/java/io/github/mitohondriyaa/notification/service/NotificationService.java
+++ src/main/java/io/github/mitohondriyaa/notification/service/NotificationService.java
@@ -7,6 +7,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,38 +18,45 @@ public class NotificationService {
     private final RedisService redisService;
 
     @KafkaListener(topics = "inventory-reserved")
-    public void orderPlaced(InventoryReservedEvent inventoryReservedEvent) {
-        MimeMessagePreparator messagePreparator = mimeMessage -> {
-            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+    public void orderPlaced(
+        @Payload InventoryReservedEvent inventoryReservedEvent,
+        @Header("messageId") String messageId
+    ) {
+        if (!redisService.hasKey(messageId)) {
+            MimeMessagePreparator messagePreparator = mimeMessage -> {
+                MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
 
-            messageHelper.setFrom("mitohondriyaa@gmail.com");
-            messageHelper.setTo(inventoryReservedEvent.getEmail().toString());
-            messageHelper.setSubject(
-                String.format(
-                    "Your order #%s has been placed successfully!",
-                    inventoryReservedEvent.getOrderNumber()
-                )
-            );
-            messageHelper.setText(
-                String.format(
-                    """
-                        Hi, %s %s,
-                        
-                        Thank you for your order!
-                        
-                        Your order #%s has been placed successfully!
-                        
-                        Best regards,
-                        Mitohondriyaa
-                        """,
-                    inventoryReservedEvent.getFirstName().toString(),
-                    inventoryReservedEvent.getLastName().toString(),
-                    inventoryReservedEvent.getOrderNumber()
-                )
-            );
-        };
+                messageHelper.setFrom("mitohondriyaa@gmail.com");
+                messageHelper.setTo(inventoryReservedEvent.getEmail().toString());
+                messageHelper.setSubject(
+                    String.format(
+                        "Your order #%s has been placed successfully!",
+                        inventoryReservedEvent.getOrderNumber()
+                    )
+                );
+                messageHelper.setText(
+                    String.format(
+                        """
+                            Hi, %s %s,
+                            
+                            Thank you for your order!
+                            
+                            Your order #%s has been placed successfully!
+                            
+                            Best regards,
+                            Mitohondriyaa
+                            """,
+                        inventoryReservedEvent.getFirstName().toString(),
+                        inventoryReservedEvent.getLastName().toString(),
+                        inventoryReservedEvent.getOrderNumber()
+                    )
+                );
+            };
 
-        mailSender.send(messagePreparator);
+            mailSender.send(messagePreparator);
+
+            redisService.setValue(messageId);
+        }
     }
 
     @KafkaListener(topics = "inventory-rejected")
